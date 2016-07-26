b039434a21e897eebf39aee34c0710ccaed4ee77
MATTHEW NGAI <MTFNGAI@uwaterloo.ca>
2016-07-25 23:34:45 -0400

    adding frontend

--- index.js
+++ index.js
@@ -1,62 +1,19 @@
-// var margin = {top: 20, right: 20, bottom: 30, left: 50},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
+var React    = window.React = require('react'),
+    App;
 
-// var formatDate = d3.timeFormat("%d-%b-%y");
+App = React.createClass({
+    render: function () {
+        return <div>
+            <Header/>
+            <div className="container content">
+                <Posts/>
+            </div>
+        </div>;
+    }
+});
 
-// var x = d3.time.scale()
-//     .range([0, width]);
+App.start = function () {
+    React.render(<App/>, document.getElementById('app'));
+};
 
-// var y = d3.scale.linear()
-//     .range([height, 0]);
-
-// var xAxis = d3.svg.axis()
-//     .scale(x)
-//     .orient("bottom");
-
-// var yAxis = d3.svg.axis()
-//     .scale(y)
-//     .orient("left");
-
-// var line = d3.svg.line()
-//     .x(function(d) { return x(d.date); })
-//     .y(function(d) { return y(d.close); });
-
-// var svg = d3.select("body").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// d3.tsv("data.tsv", type, function(error, data) {
-//   if (error) throw error;
-
-//   x.domain(d3.extent(data, function(d) { return d.date; }));
-//   y.domain(d3.extent(data, function(d) { return d.close; }));
-
-//   svg.append("g")
-//       .attr("class", "x axis")
-//       .attr("transform", "translate(0," + height + ")")
-//       .call(xAxis);
-
-//   svg.append("g")
-//       .attr("class", "y axis")
-//       .call(yAxis)
-//     .append("text")
-//       .attr("transform", "rotate(-90)")
-//       .attr("y", 6)
-//       .attr("dy", ".71em")
-//       .style("text-anchor", "end")
-//       .text("Price ($)");
-
-//   svg.append("path")
-//       .datum(data)
-//       .attr("class", "line")
-//       .attr("d", line);
-// });
-
-// function type(d) {
-//   d.date = formatDate.parse(d.date);
-//   d.close = +d.close;
-//   return d;
-// }
+module.exports = window.App = App;
