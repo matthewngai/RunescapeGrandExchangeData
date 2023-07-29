d6dcae1bbcdc64b1cac471f2184f3c359e38829f
Matthew Ngai <6504408+matthewngai@users.noreply.github.com>
2023-07-29 14:53:53 -0700

    added babel

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,15 @@
-// var React    = window.React = require('react'),
-//     App;
 
 var App = React.createClass({
     render: function() {
-        console.log('hello');
         return (
             <div>
-            hello
+                Hello!
             </div>
         )
     }
 });
 
-// App.start = function () {
-//     React.render(<App/>, document.getElementById('app'));
-// };
-
-React.render(
+ReactDOM.render(
     <App />,
     document.getElementById('app')
 );
